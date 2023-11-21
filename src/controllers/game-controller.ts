8888dd8fc8280de813de3301dbf0fedab0ae6b33
nilteras <niltonrnascimento@hotmail.com>
2023-11-21 15:08:46 -0300

    chore: delete .envs

--- src/controllers/game-controller.ts
+++ src/controllers/game-controller.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { gamesService } from "./../services/game-service";
+import { CreateGame, Score } from "protocols";
 
 export async function createGame(req: Request, res: Response) {
-    const { homeTeamName, awayTeamName } = req.body;
+    const { homeTeamName, awayTeamName } = req.body as CreateGame;
 
     const result = await gamesService.postGame({ homeTeamName, awayTeamName });
 
@@ -27,7 +28,7 @@ export async function getGameWithBets(req: Request, res: Response) {
 export async function finishGame(req: Request, res: Response, next: NextFunction) {
     try {
         const gameId = req.params.id;
-        const { homeTeamScore, awayTeamScore } = req.body;
+        const { homeTeamScore, awayTeamScore } = req.body as Score;
 
         const result = await gamesService.finishGame(Number(gameId), { homeTeamScore, awayTeamScore });
 
